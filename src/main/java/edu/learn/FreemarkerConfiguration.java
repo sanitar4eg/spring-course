8426017f0b40f280f86f060743869a6e234ab3fa
Denis_Mezhuev <Denis_Mezhuev@epam.com>
2017-07-10 22:12:23 +0400

    Migrate to custom FreeMarkerViewResolver

---
+++ src/main/java/edu/learn/FreemarkerConfiguration.java
@@ -0,0 +1,35 @@
+package edu.learn;
+
+import freemarker.template.TemplateException;
+import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+public class FreemarkerConfiguration {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+		resolver.setCache(true);
+		resolver.setPrefix("");
+		resolver.setSuffix(".ftl");
+		resolver.setContentType("text/html; charset=UTF-8");
+		return resolver;
+	}
+
+	@Bean
+	public FreeMarkerConfigurer freemarkerConfig() throws IOException, TemplateException {
+		FreeMarkerConfigurationFactory factory = new FreeMarkerConfigurationFactory();
+		factory.setTemplateLoaderPath("classpath:templates");
+		factory.setDefaultEncoding("UTF-8");
+		FreeMarkerConfigurer result = new FreeMarkerConfigurer();
+		result.setConfiguration(factory.createConfiguration());
+		return result;
+	}
+
+}
